2bcb217225e6ca3f2a675869d7a0de990449a853
kohara321 <kohara@DESKTOP-VIORR1O.localdomain>
2025-03-02 21:31:16 -0700

    fetch movie data and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import Home from './components/sections/Home';
 import Navbar from './components/Navbar';
 import Favorites from './components/sections/Favorites';
 import WatchPage from './components/sections/WatchPage';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const favoriteMovies = [
-    { id: 1, title: 'Movie 1', description: 'Description for Movie 1', imageUrl: 'https://picsum.photos/200/300' },
-    { id: 2, title: 'Movie 2', description: 'Description for Movie 2', imageUrl: 'https://picsum.photos/200/300' },
-    { id: 3, title: 'Movie 3', description: 'Description for Movie 3', imageUrl: 'https://picsum.photos/200/300' },
-  ]
+  const movieData = async (movie) => {
+    try {
+      const BASE_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=dcbc1f23&t=${movie}&plot=full`;
+      const response = await axios.get(BASE_URL);
+      console.log("Response Status:", response.status);
+      console.log("Response Data:", response.data);
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error occurred during API call:", error);
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    movieData("the dark knight");
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites movies={favoriteMovies} />} />
-        <Route path='/watch' element={<WatchPage />}/>
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path='/watch' element={<WatchPage movies={data} />}/>
       </Routes>
     </div>
   );
--- src/components/sections/Favorites.jsx
+++ src/components/sections/Favorites.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import MovieCard from '../MovieCard'
 
-const Favorites = ({ movies }) => {
+const Favorites = () => {
 
   return (
-    <div className='min-h-screen w-full relative flex flex-col justify-center items-center text-center border-2 [border-image:linear-gradient(to_top,#83C59B,#FCF09A,red,purple,#71CAEE)_1] bg-black'>
-      <h1 className='text-white'>Favorites Page</h1>
-      {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+    <div className='min-h-screen w-full flex justify-center items-center text-center gap-[10%] border-2 [border-image:linear-gradient(to_top,#83C59B,#FCF09A,red,purple,#71CAEE)_1] bg-black'>
+
     </div>
   )
 }
